80bb3c84516de7732f439355909b212e982684ce
abhayk8717-dev <abhayk8717@gmail.com>
2026-05-26 22:38:42 +0530

    Update1 rg.tf

--- rg.tf
+++ rg.tf
@@ -1,3 +1,7 @@
 resource "azurerm_resource_group" "RG Creation"
 name = "Rg-Apple"
 location = "Central India"
+
+resource "azurerm_resource_group" "RG Creation1"
+name = "Rg-Orange"
+location = "Central India"
